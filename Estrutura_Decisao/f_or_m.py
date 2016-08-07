6031d40ad537f0d24e6bf1ef9e2b7172490c27d2
M3nin0 <phelpsmemo@gmail.com>
2016-08-07 18:21:25 -0300

    Exercicios de estrutura de decisao

---
+++ Estrutura_Decisao/f_or_m.py
@@ -0,0 +1,8 @@
+letra = input("Insira umas das letras\nM - Masculino\nF - Feminino\n:")
+
+if letra == "M":
+    print ("Masculino")
+elif letra == "F":
+    print ("Feminino")
+else:
+    print ("Caractere Invalido!!!")
---
+++ Estrutura_Decisao/the_biggest.py
@@ -0,0 +1,7 @@
+num1 = int(input("Insira o primeiro numero: "))
+num2 = int(input("Insira o segundo numero: "))
+
+if num1 > num2:
+    print ("O maior número é:",num1)
+else:
+    print ("O maior número é:",num2)
