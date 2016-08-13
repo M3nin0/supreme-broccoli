5687164fce957da1105ea7ff712bfa55021c0487
M3nin0 <phelpsmemo@gmail.com>
2016-08-13 19:25:29 -0300

    Exercício - Semana

---
+++ Estrutura_Decisao/week.py
@@ -0,0 +1,18 @@
+dia = int(input("Insira o dia da semana\n1 - Segunda-feira\n2 - Terça-feira\n3 - Quarta-feira\n4 - Quinta-feira\n5 - Sexta-feita\n6 - Sabado\n7 - Domingo\n:"))
+
+if dia == 1:
+	print("Olá Segunda-Feira")
+elif dia == 2:
+	print("Olá Terça-Feira")
+elif dia == 3:
+	print("Olá Quarta-Feira")
+elif dia == 4:
+	print("Olá Quinta-Feira")
+elif dia == 5:
+	print("Olá Sexta-Feira")
+elif dia == 6:
+	print("Olá Sabado")
+elif dia == 7:
+	print("Olá Domingo")
+else:
+	print("INVALIDO!!! TENTE NOVAMENTE")
