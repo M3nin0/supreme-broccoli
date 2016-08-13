a5ccb8aaa59c6dd5bcdd124f5c2fd37774e71192
M3nin0 <phelpsmemo@gmail.com>
2016-08-13 18:22:38 -0300

    Mais estrutura de decisão

---
+++ Estrutura_Decisao/good_day_or_night.py
@@ -0,0 +1,11 @@
+per = input("Insira o periodo do dia\nM - Matutino\nV - Vespertino\nN - Noturno\n: ")
+
+if per == "M":
+	print("Bom Dia")
+elif per == "V":
+	print("Boa Tarde")
+elif per == "N":
+	print("Boa noite")
+else:
+	print("Valor invalido")
+	
