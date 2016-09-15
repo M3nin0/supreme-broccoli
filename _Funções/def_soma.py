9567bc54da76d4c480f2d5aae699202d17e812f7
M3nin0 <phelpsmemo@gmail.com>
2016-09-14 21:48:09 -0300

    Iniciando exercícios de função

---
+++ _Funções/def_soma.py
@@ -0,0 +1,10 @@
+# Função simples de somas
+
+def soma(a,b,c):
+	print("A soma é igual a:", a + b + c)
+
+a = int(input("Insira o valor da primeira variavel: "))
+b = int(input("Insira o valor da segunda variavel: "))
+c = int(input("Insira o valor da terceira variavel: "))
+
+soma(a,b,c)
